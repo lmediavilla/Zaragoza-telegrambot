00ebab91b8b5e6ea0e73e4cfb42e8f44fe3ebb76
Luis Mediavilla <lmediavilla@outlook.com>
2018-10-05 20:04:29 +0200

    inicial

---
+++ modules/bicizgz/bici.py
@@ -0,0 +1,15 @@
+import conexion
+import listaparada
+import posicionUsuario
+
+class bici:
+
+    def __init__(self):
+        print('init')
+
+def main():
+        c = bici()
+        del c
+
+if __name__ == '__main__':
+    main()
---
+++ modules/bicizgz/conexion.py
@@ -0,0 +1,5 @@
+
+def main():
+    print('void')
+if __name__ == '__main__':
+    main()
