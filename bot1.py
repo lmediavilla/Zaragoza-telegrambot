6ae190eadd2aba44b6d33334e441616dfd49a0a8
Luis Mediavilla <lmediavilla@outlook.com>
2018-10-14 23:32:29 +0200

    inlinebot
    inline bot envia la localización de las paradas

--- bot1.py
+++ bot1.py
@@ -1,24 +1,13 @@
 # coding utf-8
-from telegram.ext import CommandHandler, Updater, MessageHandler, Filters
-import telegram
+from telegram import InlineQueryResultArticle, InputTextMessageContent, InlineQueryResultLocation, InlineQueryResult
+from telegram.ext import CommandHandler, Updater, MessageHandler, Filters, InlineQueryHandler
+from telegram.ext.dispatcher import run_async
 from functools import wraps
 from sys import argv
 import logging
 
-
-
-def test(bot, update):
-    bot.send_chat_action(update.message.chat_id, action=telegram.ChatAction.TYPING)
-    location_keyboard = telegram.KeyboardButton(text="enviame la posicion", request_location=True)
-    custom_keyboard = [[ location_keyboard]]
-    reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard)
-    bot.send_message(update.message.chat_id, text="me compartes la hubicación?", reply_markup=reply_markup)
-    telegram.ForceReply(force_reply=True, selective=False)
-    logging.info(f'update.message.chat_id -> {update.message.chat_id} update.message.from_user ->  {update.message.from_user}')
-
 def start(bot, update):
-    bot.send_chat_action(update.message.chat_id, action=telegram.ChatAction.TYPING)
-    bot.send_message(update.message.chat_id, text="soporto /test")
+    bot.send_message(update.message.chat_id, text="espero que te guste")
 
 def token():
     #lee el token de fichero
@@ -30,25 +19,29 @@ def token():
     logging.info(f'API1 -> {API1}')
     return API1
 
-def echo_text(bot, update):
-    logging.info(f'update.message.text -> {update.message.text} \nupdate.message.chat_id -> {update.message.chat_id} \nupdate.message.from_user ->  {update.message.from_user}')
-    bot.send_message(update.message.chat_id, f'update.message.text -> {update.message.text} \nupdate.message.chat_id -> {update.message.chat_id} \nupdate.message.from_user ->  {update.message.from_user}')
-
-def echo_location(bot, update):
-    logging.info(f'update.message.location -> {update.message.location} \nupdate.message.chat_id -> {update.message.chat_id} \nupdate.message.from_user ->  {update.message.from_user}')
-    MapaLoingitud = update.message.location.longitude
-    MapaLatitud = update.message.location.latitude
-    bot.send_message(update.message.chat_id, f'MapaLoingitud -> {MapaLoingitud} \nMapaLatitud -> {MapaLatitud}')
-    bot.send_message(update.message.chat_id, f'update.message.location -> {update.message.location} \nupdate.message.chat_id -> {update.message.chat_id} \nupdate.message.from_user ->  {update.message.from_user}')
+@run_async
+def trabaja(bot, update):
+    logging.info("trabaja(bot, update)")
+    loc0 = InlineQueryResultLocation(1,latitude=41.120689, longitude=1.1810739, title='bicis=1 huecos=3 {calle}')
+    loc2 = InlineQueryResultLocation(2,latitude=43.4618219, longitude=-3.8112334, title='bicis=2 huecos=1 {calle}')
+    #result = Bici.paradascercanas(update.inline_query.location.longitude, update.inline_query.location.latitude, nparadas)
+    results = list()
+    results.append(loc0)
+    results.append(loc2)
+    #loc01 = InlineQueryResultArticle(id="3",title="Parada 1",input_message_content=InputTextMessageContent('bicis={nbicis} huecos={nhuecos} calle {calle}',parse_mode="HTML"), url="https://www.google.com/maps/place/La+Canonja,+Tarragona/@41.1201774,1.181502")
+    #loc02 = InlineQueryResultArticle(id="4", title="Parada 2", input_message_content=InputTextMessageContent("bicis={nbicis} huecos={nhuecos} calle {calle}",parse_mode="HTML"), url='https://www.google.com/maps/place/Santander,+Cantabria/@43.4623256,-3.8100945')
+    #results.append(loc01)
+    #results.append(loc02)
+    bot.answerInlineQuery(update.inline_query.id, results, cache_time=0) 
+    logging.info(f'update.inline_query.location.longitude -> {update.inline_query.location.longitude}')
+    logging.info(f'update.inline_query.location.latitude -> {update.inline_query.location.latitude}')
 
 def main(args):
     logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
     API1 = token()
-    updater = Updater(API1)
+    updater = Updater(API1, workers=10)
     dispatcher = updater.dispatcher
-    dispatcher.add_handler(MessageHandler(Filters.text,echo_text))
-    dispatcher.add_handler(MessageHandler(Filters.location,echo_location))
-    dispatcher.add_handler(CommandHandler('test',test,pass_args = False))
+    dispatcher.add_handler(InlineQueryHandler(trabaja))
     updater.start_polling(clean=True)
     updater.idle()
 
