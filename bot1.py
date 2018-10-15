c7e7d3fdab20974e17f1def6925a55e6e0075ed4
Luis Mediavilla <lmediavilla@outlook.com>
2018-10-16 01:42:03 +0200

    mejora maps

--- bot1.py
+++ bot1.py
@@ -5,6 +5,9 @@ from telegram.ext.dispatcher import run_async
 from functools import wraps
 from sys import argv
 import logging
+import sys
+import modules.bicizgz
+from modules.bicizgz.bici import bici
 
 def start(bot, update):
     bot.send_message(update.message.chat_id, text="espero que te guste")
@@ -24,11 +27,14 @@ def trabaja(bot, update):
     logging.info("trabaja(bot, update)")
     logging.info(f'update.inline_query.query -> {update.inline_query.query}')
     results = list()
+    logging.info(f'update.inline_query.location.longitude -> {update.inline_query.location.longitude}')
+    logging.info(f'update.inline_query.location.latitude -> {update.inline_query.location.latitude}')
     if not update.inline_query.query:
         results.append(InlineQueryResultArticle(id="3",title="comando vacio escribe bici o bus o tranvía",input_message_content=InputTextMessageContent('escribe bici o bus o tranvia',parse_mode="HTML"), ))
         bot.answerInlineQuery(update.inline_query.id, results, cache_time=0)
         return
     elif (update.inline_query.query == 'bici'):
+        '''
         loc0 = InlineQueryResultLocation(1,latitude=41.120689, longitude=1.1810739, title='bicis=1 huecos=3 {calle}')
         loc2 = InlineQueryResultLocation(2,latitude=43.4618219, longitude=-3.8112334, title='bicis=2 huecos=1 {calle}')
         #result = Bici.paradascercanas(update.inline_query.location.longitude, update.inline_query.location.latitude, nparadas)        
@@ -41,6 +47,27 @@ def trabaja(bot, update):
         bot.answerInlineQuery(update.inline_query.id, results, cache_time=0) 
         logging.info(f'update.inline_query.location.longitude -> {update.inline_query.location.longitude}')
         logging.info(f'update.inline_query.location.latitude -> {update.inline_query.location.latitude}')
+        '''
+        MiBici = bici()
+        bot.answerInlineQuery(update.inline_query.id, MiBici.obtenlistaDistanciaUsuario(float(0),str(update.inline_query.location.latitude),str(update.inline_query.location.longitude)), cache_time=0) 
+        return
+    elif (update.inline_query.query == 'bicis'):
+        '''
+        loc0 = InlineQueryResultLocation(1,latitude=41.120689, longitude=1.1810739, title='bicis=1 huecos=3 {calle}')
+        loc2 = InlineQueryResultLocation(2,latitude=43.4618219, longitude=-3.8112334, title='bicis=2 huecos=1 {calle}')
+        #result = Bici.paradascercanas(update.inline_query.location.longitude, update.inline_query.location.latitude, nparadas)        
+        results.append(loc0)
+        results.append(loc2)
+        #loc01 = InlineQueryResultArticle(id="3",title="Parada 1",input_message_content=InputTextMessageContent('bicis={nbicis} huecos={nhuecos} calle {calle}',parse_mode="HTML"), url="https://www.google.com/maps/place/La+Canonja,+Tarragona/@41.1201774,1.181502")
+        #loc02 = InlineQueryResultArticle(id="4", title="Parada 2", input_message_content=InputTextMessageContent("bicis={nbicis} huecos={nhuecos} calle {calle}",parse_mode="HTML"), url='https://www.google.com/maps/place/Santander,+Cantabria/@43.4623256,-3.8100945')
+        #results.append(loc01)
+        #results.append(loc02)
+        bot.answerInlineQuery(update.inline_query.id, results, cache_time=0) 
+        logging.info(f'update.inline_query.location.longitude -> {update.inline_query.location.longitude}')
+        logging.info(f'update.inline_query.location.latitude -> {update.inline_query.location.latitude}')
+        '''
+        MiBici = bici()
+        bot.answerInlineQuery(update.inline_query.id, MiBici.obtenlistaDistanciaUsuario2(float(0),str(update.inline_query.location.latitude),str(update.inline_query.location.longitude)), cache_time=0) 
         return
     elif (update.inline_query.query == 'bus'):
         loc0 = InlineQueryResultLocation(1,latitude=41.120689, longitude=1.1810739, title='bicis=1 huecos=3 {calle}')
@@ -53,8 +80,6 @@ def trabaja(bot, update):
         #results.append(loc01)
         #results.append(loc02)
         bot.answerInlineQuery(update.inline_query.id, results, cache_time=0) 
-        logging.info(f'update.inline_query.location.longitude -> {update.inline_query.location.longitude}')
-        logging.info(f'update.inline_query.location.latitude -> {update.inline_query.location.latitude}')
         return
     elif (update.inline_query.query == 'tranvia'):
         loc0 = InlineQueryResultLocation(1,latitude=41.120689, longitude=1.1810739, title='bicis=1 huecos=3 {calle}')
@@ -67,8 +92,6 @@ def trabaja(bot, update):
         #results.append(loc01)
         #results.append(loc02)
         bot.answerInlineQuery(update.inline_query.id, results, cache_time=0) 
-        logging.info(f'update.inline_query.location.longitude -> {update.inline_query.location.longitude}')
-        logging.info(f'update.inline_query.location.latitude -> {update.inline_query.location.latitude}')
         return
     else:
         results.append(InlineQueryResultArticle(id="3",title="comando no soportado",input_message_content=InputTextMessageContent('escribe bici o bus o tranvia',parse_mode="HTML"), ))
--- modules/bicizgz/bici.py
+++ modules/bicizgz/bici.py
@@ -1,7 +1,10 @@
-import conexion
-import listaparada
-import posicionUsuario
+import sys
+from modules.bicizgz import conexion
+import modules.bicizgz.listaparada
+from modules.bicizgz.posicionUsuario import obtenerlistaPosicionUsuario
 import pandas as pd
+from telegram import InlineQueryResultArticle, InlineQueryResultLocation, InputTextMessageContent
+import logging
 
 class bici:
 
@@ -12,12 +15,12 @@ class bici:
     def obtenlistaParadas(self):
         return conexion.obtenlistaParadas()
 
-    def obtenlistaDistanciaUsuario(self, listaparadas, idUsuario='0', LatitudeUsuario='0', LongitudeUsuario='0'):
-        listakm = posicionUsuario.obtenerlistaPosicionUsuario(listaparadas, idUsuario, LatitudeUsuario, LongitudeUsuario)
+    def obtenlistaDistanciaUsuario(self, idUsuario='0', LatitudeUsuario='0', LongitudeUsuario='0'):
+        listaparadas = pd.read_csv('modules/bicizgz/paradas.csv', sep=',',header=0)
+        listakm = obtenerlistaPosicionUsuario(listaparadas, idUsuario, LatitudeUsuario, LongitudeUsuario)
         listaresult = []
 
         for index, fila in listakm.iterrows():
-
            #------------------------------------------------------------------------------------------------------# 
            #COLUMNAS DEL DATAFRAME DEL CALCULO DE DISTANCIAS                                                      #
            #[Distancia, UsuarioID, ParadaID, LatitudeUsuario, LongitudeUsuario, LatitudeParada, LongitudeParada]  #
@@ -30,28 +33,53 @@ class bici:
            #DATOS A RETORNAR EN UNA LISTA                                                                         #
            #[2,latitude=43.4618219, longitude=-3.8112334, title='bicis=2 huecos=1 {calle}']                       #
            #------------------------------------------------------------------------------------------------------#
-
-           filtro = listaparadas[listaparadas['StationID'] == fila['ParadaID']]
-           listaresult.append([
-                                str(filtro['StationID'].values),
-                                str(filtro['AddressGmapsLatitude'].values),
-                                str(filtro['AddressGmapsLongitude'].values),
-                                str(filtro['StationAvailableBikes'].values) + " " + \
-                                str(filtro['StationFreeSlot'].values) + " {" + str(filtro['AddressStreet1'].values) +"}"
-                              ])
-
+            filtro = listaparadas[listaparadas['StationID'] == fila['ParadaID']]
+            listaresult.append(InlineQueryResultLocation((filtro['StationID']).values,    \
+                            float(filtro['AddressGmapsLatitude']),    \
+                            float(filtro['AddressGmapsLongitude']),    \
+                            title=f"{filtro['StationAvailableBikes'].values} bicis {filtro['StationFreeSlot'].values} huecos {filtro['AddressStreet1'].values}".replace('[','').replace(']','')))
+            if len(listaresult) == 6:
+                logging.info('6 estaciones enviadas')
+                break
         return listaresult
-          
-          
-        
 
+    def obtenlistaDistanciaUsuario2(self, idUsuario='0', LatitudeUsuario='0', LongitudeUsuario='0'):
+        listaparadas = pd.read_csv('modules/bicizgz/paradas.csv', sep=',',header=0)
+        listakm = obtenerlistaPosicionUsuario(listaparadas, idUsuario, LatitudeUsuario, LongitudeUsuario)
+        listaresult = []
+        ApiKey='LEE EL API KEY DE TRELLO'
+        for index, fila in listakm.iterrows():
+           #------------------------------------------------------------------------------------------------------# 
+           #COLUMNAS DEL DATAFRAME DEL CALCULO DE DISTANCIAS                                                      #
+           #[Distancia, UsuarioID, ParadaID, LatitudeUsuario, LongitudeUsuario, LatitudeParada, LongitudeParada]  #
+           #------------------------------------------------------------------------------------------------------# 
+           #COLUMNAS DEL DATAFRAME OBTENIDOS DEL SERVIDOR                                                         #
+           #[StationID, DisctrictCode, AddressGmapsLatitude, AddressGmapsLongitude, StationAvailableBikes   \     #
+           # StationFreeSlot, AddressZipCode, AddressStreet1, AddressNumber, NearbyStationList,             \     #
+           # StationStatusCode, StationName]                                                                      #
+           #------------------------------------------------------------------------------------------------------# 
+           #DATOS A RETORNAR EN UNA LISTA                                                                         #
+           #[2,latitude=43.4618219, longitude=-3.8112334, title='bicis=2 huecos=1 {calle}']                       #
+           #------------------------------------------------------------------------------------------------------#
+            filtro = listaparadas[listaparadas['StationID'] == fila['ParadaID']]
+            thumb=f"https://maps.googleapis.com/maps/api/staticmap?center={filtro['AddressGmapsLatitude'].values},{filtro['AddressGmapsLongitude'].values}&zoom=20&size=600x400&key={ApiKey}".replace('[','').replace(']','')
+            logging.info(f'url -> {thumb}')
+            add = InlineQueryResultArticle((filtro['StationID']).values,    \
+                                            title=f"{filtro['StationAvailableBikes'].values}b/{filtro['StationFreeSlot'].values}h {filtro['AddressStreet1'].values}".replace('[','').replace(']',''),    \
+                                            input_message_content=InputTextMessageContent(f"{filtro['StationAvailableBikes'].values} bicis {filtro['StationFreeSlot'].values} huecos \n\r{filtro['AddressStreet1'].values}\r\nhttps://www.google.com/maps/@{float(filtro['AddressGmapsLatitude'])},{float(filtro['AddressGmapsLongitude'])},20z ".replace('[','').replace(']',''),parse_mode="HTML"),    \
+                                            thumb_url=thumb,thumb_width=600,thumb_height=400)
+            listaresult.append(add)
+            if len(listaresult) == 6:
+                logging.info('6 estaciones enviadas')
+                break
+        return listaresult
 
 def main():
     c = bici()
     print(c.obtenlistaParadas())
 
     lp = pd.read_csv('paradas.csv', sep=',',header=0)
-    print(f'{c.obtenlistaDistanciaUsuario( lp, "1234567", "41.6218009", "-0.9523861")}')
+    print(f'{c.obtenlistaDistanciaUsuario( "1234567", "41.6218009", "-0.9523861")}')
 
     del c
 
