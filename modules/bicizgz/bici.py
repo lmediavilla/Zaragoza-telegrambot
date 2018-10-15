e1c24a548b2e8d126304c25d980bd0b7d33f6065
labsturtles <labsturtles@gmail.com>
2018-10-15 22:23:46 +0200

    Actualizacion de modulos
    Modificacion de las funciones de posicion

--- modules/bicizgz/bici.py
+++ modules/bicizgz/bici.py
@@ -13,7 +13,34 @@ class bici:
         return conexion.obtenlistaParadas()
 
     def obtenlistaDistanciaUsuario(self, listaparadas, idUsuario='0', LatitudeUsuario='0', LongitudeUsuario='0'):
-        return posicionUsuario.obtenerlistaPosicionUsuario(listaparadas, idUsuario, LatitudeUsuario, LongitudeUsuario)
+        listakm = posicionUsuario.obtenerlistaPosicionUsuario(listaparadas, idUsuario, LatitudeUsuario, LongitudeUsuario)
+        listaresult = []
+
+        for index, fila in listakm.iterrows():
+
+           #------------------------------------------------------------------------------------------------------# 
+           #COLUMNAS DEL DATAFRAME DEL CALCULO DE DISTANCIAS                                                      #
+           #[Distancia, UsuarioID, ParadaID, LatitudeUsuario, LongitudeUsuario, LatitudeParada, LongitudeParada]  #
+           #------------------------------------------------------------------------------------------------------# 
+           #COLUMNAS DEL DATAFRAME OBTENIDOS DEL SERVIDOR                                                         #
+           #[StationID, DisctrictCode, AddressGmapsLatitude, AddressGmapsLongitude, StationAvailableBikes   \     #
+           # StationFreeSlot, AddressZipCode, AddressStreet1, AddressNumber, NearbyStationList,             \     #
+           # StationStatusCode, StationName]                                                                      #
+           #------------------------------------------------------------------------------------------------------# 
+           #DATOS A RETORNAR EN UNA LISTA                                                                         #
+           #[2,latitude=43.4618219, longitude=-3.8112334, title='bicis=2 huecos=1 {calle}']                       #
+           #------------------------------------------------------------------------------------------------------#
+
+           filtro = listaparadas[listaparadas['StationID'] == fila['ParadaID']]
+           listaresult.append([
+                                str(filtro['StationID']),
+                                str(filtro['AddressGmapsLatitude']),
+                                str(filtro['AddressGmapsLongitude']),
+                                str(filtro['StationAvailableBikes']) + " " + \
+                                str(filtro['StationFreeSlot']) + " {" + str(filtro['AddressStreet1']) +"}"
+                              ])
+
+        return listaresult
           
           
         
@@ -24,7 +51,7 @@ def main():
     print(c.obtenlistaParadas())
 
     lp = pd.read_csv('paradas.csv', sep=',',header=0)
-    print(c.obtenlistaDistanciaUsuario( lp, "1234567", "41.6218009", "-0.9523861",))
+    print(f'{c.obtenlistaDistanciaUsuario( lp, "1234567", "41.6218009", "-0.9523861")}')
 
     del c
 
--- modules/bicizgz/posicionUsuario.py
+++ modules/bicizgz/posicionUsuario.py
@@ -1,5 +1,8 @@
 import math as m
 import pandas as df
+import googlemaps as ggmp
+from datetime import datetime
+import logging
 
 def obtenerlistaPosicionUsuario(listaparadas, idUsuario, LatitudeUsuario, LongitudeUsuario):
     
@@ -8,38 +11,68 @@ def obtenerlistaPosicionUsuario(listaparadas, idUsuario, LatitudeUsuario, Longit
 
         for index, fila in listaparadas.iterrows():
             
-            data.loc[len(data)]=[  
-                            calcula_distancia(LatitudeUsuario, LongitudeUsuario, \
-                                              fila['AddressGmapsLatitude'], fila['AddressGmapsLongitude']),
-                            idUsuario,
-                            fila['StationID'],
-                            fila['AddressGmapsLatitude'], 
-                            fila['AddressGmapsLongitude'],
-                            LatitudeUsuario, 
-                            LongitudeUsuario
-                            
-            ]  
+            if fila['StationStatusCode']=='OPN':
+                    data.loc[len(data)]=[  
+                                    calcula_distancia(LatitudeUsuario, LongitudeUsuario, \
+                                                    fila['AddressGmapsLatitude'], fila['AddressGmapsLongitude']),
+                                    idUsuario,
+                                    fila['StationID'],
+                                    fila['AddressGmapsLatitude'], 
+                                    fila['AddressGmapsLongitude'],
+                                    LatitudeUsuario, 
+                                    LongitudeUsuario
+                                    
+                    ]  
             
         return data.sort_values(by=['Distancia'], ascending=[True])
 
 
+def acomodador(strLatitude='0', strLongitude='0'):
+    
+    try:
+            Latitude=float(strLatitude.replace(',','.'))
+            Longitude=float(strLongitude.replace(',','.'))
+    except:
+            Latitude=float(strLatitude)
+            Longitude=float(strLongitude)
+    
+    return Latitude, Longitude
+
+
+def token():
+    #lee el token de fichero
+    tGM = ''
+    print("TKn1", tGM)
+    with open('./token.txt', 'rU') as f:
+        tGM = f.readline()
+        tGM = tGM.rstrip('\n')
+        f.close()
+    logging.info(f'tGM -> {tGM}')
+    print("TKn", tGM)
+    return tGM
+
+
+def calcula_tiempodistancia(strLatitudeOrigen='0', strLongitudeOrigen='0',    \
+                            strLatitudeDestino='0', strLongitudeDestino='0'):
+    now = datetime.now()
+    token()
+    """
+    gmaps = ggmp.Client(key=token())
+    directions_result = gmaps.directions(str(acomodador(strLatitudeOrigen, strLongitudeOrigen)),
+                                         str(acomodador(strLatitudeDestino, strLongitudeDestino)),
+                                          mode="walking",
+                                          departure_time=now)
+"""
+    print ("RESULTADO  ", directions_result)                                      
+    return directions_result
+
 def calcula_distancia(strLatitudeOrigen='0', strLongitudeOrigen='0',    \
-                          strLatitudeDestino='0', strLongitudeDestino='0'):
+                      strLatitudeDestino='0', strLongitudeDestino='0'):
 
         RADIO_TIERRA = 6371
 
-        try:
-            LatitudeOrigen=float(strLatitudeOrigen.replace(',','.'))
-            LongitudeOrigen=float(strLongitudeOrigen.replace(',','.'))
-        except:
-            LatitudeOrigen=float(strLatitudeOrigen)
-            LongitudeOrigen=float(strLongitudeOrigen)
-        try:
-            LatitudeDestino=float(strLatitudeDestino.replace(',','.'))
-            LongitudeDestino=float(strLongitudeDestino.replace(',','.'))
-        except:
-            LatitudeDestino=float(strLatitudeDestino)
-            LongitudeDestino=float(strLongitudeDestino)
+        LatitudeOrigen, LongitudeOrigen = acomodador(strLatitudeOrigen, strLongitudeOrigen)
+        LatitudeDestino, LongitudeDestino = acomodador(strLatitudeDestino, strLongitudeDestino) 
    
         Distancia_Km = RADIO_TIERRA * m.acos(
                                              m.cos(m.radians(90-LatitudeOrigen)) * m.cos(m.radians(90-LatitudeDestino)) + 
@@ -55,6 +88,7 @@ def main():
     km=calcula_distancia("41,644029", "-0,897865",  "41,6496477", "-0,8883027")
     print('Distancia: ', km) #Distancia:  1.010759
     """
+    calcula_tiempodistancia("41,644029", "-0,897865",  "41,6496477", "-0,8883027")
     pass
 
 if __name__ == '__main__':
